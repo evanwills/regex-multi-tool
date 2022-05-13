a951ef14eab9f33aa77dddc6f7656e1e6d74c4d7
Evan Wills <evan.wills@acu.edu.au>
2022-05-13 23:18:08 +1000

    improve output of build-n-deploy.sh

--- build-n-deploy.sh
+++ build-n-deploy.sh
@@ -10,7 +10,9 @@ echo 'clean up index.html'
 # Make paths relative to application root
 # rather than site root
 sed -i 's/="\/assets\//="assets\//ig' dist/index.html;
-
+sed -i 's/[ \t]*\(<link\|<script\)/\t\t\1/i' dist/index.html
+sed -i 's/[ \t]*<\/head>/\t<\/head>/i' dist/index.html;
+sed -i '/^[[:space:]]*$/d' dist/index.html
 echo;
 echo;
 echo 'deploy to Local'
